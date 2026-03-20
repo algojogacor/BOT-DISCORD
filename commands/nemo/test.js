92bba6a7198e9ba3c519fcee6498d1a627f4122f
Algojo AI <algojo@bot.com>
2026-03-20 12:17:03 +0000

    feat: tambah test by Algojo AI

---
+++ commands/nemo/test.js
@@ -0,0 +1,15 @@
+module.exports = async (command, args, msg, user, db, sock, m) => {
+    if (command !== 'test') return;
+    
+    // CARA PAKAI:
+    // • !test                -> mencoba fitur test
+    // • !test <nama>         -> test dengan nama
+    
+    let hasil = "Ini hasil dari fitur test";
+    if (args[0]) {
+        hasil += " dengan nama " + args[0];
+    }
+    await msg.reply(hasil);
+};
+
+{"status":"done","files":["commands/nemo/test.js"],"usage":"!test [nama]"}
